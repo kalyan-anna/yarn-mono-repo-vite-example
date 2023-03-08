dbee586ab7e417ece4bd22340df7caa574f39ca4
Kalyan Annamalai <kalyan.shelf@gmail.com>
2023-03-09 06:32:44 +1100

    react button

--- libs/button/Button.tsx
+++ libs/button/Button.tsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { PropsWithChildren, useState } from 'react';
 import styles from './Button.module.scss';
 
-export const Button = ({ children }) => {
-  return <button className={styles.buttonPrimary}>{children}</button>;
+export const Button: React.FC<PropsWithChildren> = ({ children }) => {
+  const [counter, setCounter] = useState(0);
+
+  const handleClick = () => {
+    setCounter((counter) => ++counter);
+  };
+
+  return (
+    <button className={styles.buttonPrimary} onClick={handleClick}>
+      {children} {counter}
+    </button>
+  );
 };
